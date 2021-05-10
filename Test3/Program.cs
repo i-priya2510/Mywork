17e16b5be6f4e89ba84211a686f25d526d6cda20
i-priya2510 <75769082+i-priya2510@users.noreply.github.com>
2021-05-10 21:52:19 +0530

    Add files via upload

---
+++ Test3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question1 question1 = new Question1();
+            //Question2 question2 = new Question2();
+            //Question3 question3 = new Question3();
+            //Question4 question4 = new Question4();
+            //Question5 question5 = new Question5();
+            //Question6 question6 = new Question6();
+            //Question7 question7 = new Question7();
+        }
+    }
+}
