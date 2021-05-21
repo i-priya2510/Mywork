9a9d175b7184f1b05966958238097238bd3ea7ea
i-priya2510 <75769082+i-priya2510@users.noreply.github.com>
2021-05-21 21:12:57 +0530

    Add files via upload

---
+++ Test 4/ProfileProject/Migration/20210521092422_First-Init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProfileProject.Migrations
+{
+    public partial class FirstInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Qualification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsEmployed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoticePeriod = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CurrentCTC = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profiles");
+        }
+    }
+}
